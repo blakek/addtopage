399ab2048c3fe173ca32d653a7634e3b997fd1e0
Blake Knight <bknight@gsandf.com>
2016-06-14 16:33:32 -0500

    initial commit

---
+++ src/addtopage.js
@@ -0,0 +1,5 @@
+export function addtopage() {
+  return 'this is a work in progress...'
+}
+
+export default addtopage
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+module.exports = {
+  context: path.resolve('./src'),
+  entry: './addtopage.js',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'addtopage.min.js'
+  },
+
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.json']
+  },
+
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    contentBase: path.resolve('./dist')
+  }
+};
