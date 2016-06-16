1d48b6df4ec50f033f7f3e36d372d85875ef68a0
Blake Knight <bknight@gsandf.com>
2016-06-16 12:49:18 -0500

    remove unused junk

--- src/addtopage.js
+++ src/addtopage.js
@@ -1,15 +1,3 @@
-export function createImageTag(src) {
-  var imageElement = document.createElement('image')
-  imageElement.src = src
-  return imageElement
-}
-
-export function createScriptTag(src) {
-  var scriptElement = document.createElement('script')
-  scriptElement.src = src
-  return scriptElement
-}
-
 export function addtopage() {
   return 'this is a work in progress...'
 }
