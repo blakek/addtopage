695bb739808a816a5f571c9c30b42f6bd06ea200
Blake Knight <bknight@gsandf.com>
2016-06-16 16:52:30 -0500

    keep tests from checking properties of undefined elements

--- test/addtopage.test.js
+++ test/addtopage.test.js
@@ -27,6 +27,7 @@ test('Creates a new image element, inferring from file extension (raster files)'
 test('Creates a new image element, inferring from file extension (SVG)', t => {
   const testSrc = 'example-image-name.svg'
   const newElement = addtopage(testSrc)
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an image element and has the correct source
   t.is(newElement.nodeName, 'IMAGE')
@@ -49,6 +50,7 @@ test('Creates a new image element using data URI, inferring from file extension'
 test('Creates a new image element without inferring from file extension', t => {
   const testSrc = 'http://localhost:3333/image.bogus-extension'
   const newElement = addtopage(testSrc, { type: 'image' })
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an image element and has the correct source
   t.is(newElement.nodeName, 'IMAGE')
@@ -58,6 +60,7 @@ test('Creates a new image element without inferring from file extension', t => {
 test('Creates a new image element using data URI without inferring from file extension', t => {
   const testSrc = 'http://localhost:3333/image.bogus-extension'
   const newElement = addtopage(testSrc, { inline: true, type: 'image' })
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an image element and has the correct source
   t.is(newElement.nodeName, 'IMAGE')
@@ -71,6 +74,7 @@ test('Creates a new image element using data URI without inferring from file ext
 test('Creates a new script element, inferring from file extension', t => {
   const testSrc = 'http://localhost:3333/hello.js'
   const newElement = addtopage(testSrc)
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an script element and has the correct source
   t.is(newElement.nodeName, 'SCRIPT')
@@ -80,6 +84,7 @@ test('Creates a new script element, inferring from file extension', t => {
 test('Creates a new script element without inferring from file extension', t => {
   const testSrc = 'http://localhost:3333/hello.bogus-extension'
   const newElement = addtopage(testSrc, { type: 'script' })
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an script element and has the correct source
   t.is(newElement.nodeName, 'SCRIPT')
@@ -90,6 +95,7 @@ test('Creates a new inline script element, inferring from file extension', async
   const testSrc = 'http://localhost:3333/hello.js'
   const scriptContents = (await axios.get(testSrc)).data
   const newElement = addtopage(testSrc, { inline: true })
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an script element and contains the same text as what's on the server
   t.is(newElement.nodeName, 'SCRIPT')
@@ -100,6 +106,7 @@ test('Creates a new inline script element without inferring from file extension'
   const testSrc = 'http://localhost:3333/hello.bogus-extension'
   const scriptContents = (await axios.get(testSrc)).data
   const newElement = addtopage(testSrc, { inline: true, type: 'script' })
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an script element and contains the same text as what's on the server
   t.is(newElement.nodeName, 'SCRIPT')
@@ -113,6 +120,7 @@ test('Creates a new inline script element without inferring from file extension'
 test('Creates a new style link element, inferring from file extension', t => {
   const testSrc = 'http://localhost:3333/style.css'
   const newElement = addtopage(testSrc)
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an script element and has the correct source
   t.is(newElement.nodeName, 'LINK')
@@ -122,6 +130,7 @@ test('Creates a new style link element, inferring from file extension', t => {
 test('Creates a new style link without inferring from file extension', t => {
   const testSrc = 'http://localhost:3333/style.bogus-extension'
   const newElement = addtopage(testSrc, { type: 'link' })
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an script element and has the correct source
   t.is(newElement.nodeName, 'LINK')
@@ -132,6 +141,7 @@ test('Creates a new inline style element, inferring from file extension', async 
   const fileSrc = 'http://localhost:3333/style.css'
   const fileContents = (await axios.get(fileSrc)).data
   const newElement = addtopage(fileSrc, { inline: true })
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an script element and contains the same text as what's on the server
   t.is(newElement.nodeName, 'STYLE')
@@ -142,6 +152,7 @@ test('Creates a new inline style element without inferring from file extension',
   const fileSrc = 'http://localhost:3333/style.bogus-extension'
   const fileContents = (await axios.get(fileSrc)).data
   const newElement = addtopage(fileSrc, { inline: true, type: 'style' })
+  if (!newElement) t.fail('Did not create an element!')
 
   // test if the element is an script element and contains the same text as what's on the server
   t.is(newElement.nodeName, 'STYLE')
