90436f265988b4ee99ba76336e03655c7ab2463f
Blake Knight <bknight@gsandf.com>
2016-06-16 17:02:25 -0500

    correct more tests; look for <link href="..." rel="stylesheet" /> for added styles

--- src/addtopage.js
+++ src/addtopage.js
@@ -1,5 +1,22 @@
-export function addtopage() {
-  return 'this is a work in progress...'
+const knownElementTypes = [
+  'image',
+  'link',
+  'script',
+  'style'
+]
+
+export function addtopage(resource, { type = undefined } = {}) {
+  if (!type) {
+    return
+  }
+
+  if (!knownElementTypes.includes(type)) {
+    return
+  }
+
+  var newElement = document.createElement(type)
+  newElement.src = resource
+  return newElement
 }
 
 export default addtopage
